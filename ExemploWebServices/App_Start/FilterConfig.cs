fd81a70200180e46fdf1f41541fa0f2fc4efb1f9
lczavaski <lczavaski@COPENGNPE00RBYG>
2017-09-06 14:07:45 -0300

    Add project files.

---
+++ ExemploWebServices/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploWebServices
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ExemploWebServices/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploWebServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("TestGitHutAPI"));
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
